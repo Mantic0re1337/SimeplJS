60994b6de2f2517207d41c70e6e207e6ec38f945
Maik Wilke <maik.wilke1997@web.de>
2024-01-06 16:00:12 +0100

    nicht zielfuehrend...

--- framework/index.js
+++
@@ -1,8 +0,0 @@
-export const init = (selector, component) => {
-    const app= document.querySelector(selector);
-    const newElement = document.createElement(component.type);
-    const newTextContent = document.createElement(component.template);
-
-    newElement.append(newTextContent);
-    app.append(newElement);
-}
--- index.js
+++
@@ -1,7 +0,0 @@
-import {init} from "./framework";
-import {div} from "./framework/element.js";
-
-const firstName = "Marvin";
-const lastName = "Frachet";
-
-init("#app", div`Hello ${firstName} ${lastName}`);
