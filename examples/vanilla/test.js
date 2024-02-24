422cf82963aab806dd2ad8b427c0b1ff6e8126c3
Maik Wilke <maik.wilke1997@web.de>
2024-02-24 20:27:49 +0100

    reconcil alg implemented and a few test

---
+++ examples/vanilla/test.js
@@ -0,0 +1,10 @@
+const obj = {
+    name: "Maik",
+    interests: {
+        hobby: "reading",
+        sport: "basketball"
+    },
+    age: 26
+}
+
+console.log(Object.keys(obj))
--- packages/runtime/src/__tests__/simplejs_sample.test.js
+++ packages/runtime/src/__tests__/simplejs_sample.test.js
@@ -1,4 +1,45 @@
 import { expect, test } from 'vitest'
+import {objectsDiff} from "../utils/objects.js";
+
+const objectDiffTestobj_old = {
+    same: "same",
+    removed: "removed",
+    modified: "notmodified"
+}
+
+const objectDiffTestobj_new = {
+    same: "same",
+    added: "added",
+    modified: "modified"
+}
+
 test('sample test', () => {
     expect(1).toBe(1)
+})
+
+test("objectsDiffDetectsAdded", () => {
+    const diff = objectsDiff(objectDiffTestobj_old, objectDiffTestobj_new)
+    const {added} = diff
+    expect(added).toEqual(["added"])
+})
+
+test("objectsDiffDetectsRemoved", () => {
+    const diff = objectsDiff(objectDiffTestobj_old, objectDiffTestobj_new)
+    const {removed} = diff
+    expect(removed).toEqual(["removed"])
+})
+
+test("objectsDiffDetectsModified", () => {
+    const diff = objectsDiff(objectDiffTestobj_old, objectDiffTestobj_new)
+    const {updated} = diff
+    expect(updated).toEqual(["modified"])
+})
+
+test("objectsDiffDetectsModified", () => {
+    const foo = {foo: "bar"}
+    const bar = {foo: "bar"}
+    const { added, removed, updated } = objectsDiff(foo, bar)
+    expect(added).toEqual([])
+    expect(removed).toEqual([])
+    expect(updated).toEqual([])
 })
